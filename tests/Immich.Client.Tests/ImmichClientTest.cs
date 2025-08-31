9b288e620f840bc56f8100b90519e16971b4863c
Luigi Grilli <luigi.grilli@gmail.com>
2025-08-31 14:30:47 +0100

    tests
    # Conflicts:
    #	tests/Directory.Build.props

--- tests/Immich.Client.Tests/ImmichClientTest.cs
+++ tests/Immich.Client.Tests/ImmichClientTest.cs
@@ -1,32 +1,37 @@
 ﻿namespace Immich.Client.Tests;
 
-public class ImmichClientTest
+public class ImmichClientTest : IDisposable
 {
-    [Fact]
-    public void Can_Create_Client()
-    {
-        using var httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri("http://localhost:2283");
-        
-        var client = new ImmichClient(httpClient);
+    private readonly HttpClient _httpClient;
+    private readonly ImmichClient _client;
 
-        Assert.NotNull(client);
+    public ImmichClientTest()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("http://localhost:2283");
+        
+        _client = new ImmichClient(_httpClient);
+
+        var response = _client.SignUpAdminAsync(new SignUpDto
+        {
+            Email = "test@test.com",
+            Name = "test",
+            Password = "test1234!&^"
+        }).Result;
+
+        Assert.Equal(UserStatus.Active, response.Status);
     }
     
     [Fact]
     public async Task Can_SignUp_Admin()
     {
-        using var httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri("http://localhost:2283");
-        
-        var client = new ImmichClient(httpClient);
-        var response = await client.SignUpAdminAsync(new SignUpDto
-        {
-            Email = "test@test.com",
-            Name = "test",
-            Password = "test1234!&^"
-        });
+        var version = await _client.GetServerVersionAsync();
+        Assert.NotNull(version);
+        Assert.Equal(1, version.Major);
+    }
 
-        Assert.Equal(UserStatus.Active, response.Status);
+    public void Dispose()
+    {
+        _httpClient.Dispose();
     }
 }
