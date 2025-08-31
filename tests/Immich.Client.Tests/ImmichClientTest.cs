8c2caff2aaa2891cdff2ebd9d58ee91c81b1af09
Luigi Grilli <luigi.grilli@gmail.com>
2025-08-31 14:13:57 +0100

    Adding admin signup

--- tests/Immich.Client.Tests/ImmichClientTest.cs
+++ tests/Immich.Client.Tests/ImmichClientTest.cs
@@ -6,11 +6,27 @@ public class ImmichClientTest
     public void Can_Create_Client()
     {
         using var httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri("http://localhost:5001");
+        httpClient.BaseAddress = new Uri("http://localhost:2283");
         
         var client = new ImmichClient(httpClient);
-        client.ApiKey = "your-api-key";
-        
+
         Assert.NotNull(client);
+    }
+    
+    [Fact]
+    public async Task Can_SignUp_Admin()
+    {
+        using var httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri("http://localhost:2283");
+        
+        var client = new ImmichClient(httpClient);
+        var response = await client.SignUpAdminAsync(new SignUpDto
+        {
+            Email = "test@test.com",
+            Name = "test",
+            Password = "test1234!&^"
+        });
+
+        Assert.Equal(UserStatus.Active, response.Status);
     }
 }
